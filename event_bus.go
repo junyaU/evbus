f1a5420f9ac27a8b12675a2de248039d22bcc45d
junya uchizono <junya19991215@icloud.com>
2022-08-08 23:57:46 +0900

    fix

--- event_bus.go
+++ event_bus.go
@@ -1,4 +1,4 @@
-package event_bus
+package evbus
 
 import (
 	"fmt"
@@ -6,9 +6,10 @@ import (
 	"sync"
 )
 
-type EBus interface {
+type Bus interface {
 	Subscribe(topic string, function interface{}) error
-	Publish(topic string, data ...interface{}) error
+	UnSubscribe(topic string, function interface{}) error
+	Publish(topic string, args ...interface{}) error
 	Wait()
 }
 
@@ -23,7 +24,7 @@ type evHandler struct {
 	callback reflect.Value
 }
 
-func New() EBus {
+func New() Bus {
 	return &EvBus{
 		handlers: make(map[string][]*evHandler),
 		lock:     sync.RWMutex{},
@@ -35,19 +36,25 @@ func (b *EvBus) Subscribe(topic string, function interface{}) error {
 		return fmt.Errorf("%s is not a reflect.Func", reflect.TypeOf(function))
 	}
 
+	reflectFunc := reflect.ValueOf(function)
+
+	if h, _ := b.findHandler(topic, reflectFunc); h != nil {
+		return fmt.Errorf("this function is already registered in %s", topic)
+	}
+
 	b.lock.Lock()
 	defer b.lock.Unlock()
 
-	h := &evHandler{
+	handler := &evHandler{
 		stream:   make(chan []reflect.Value),
-		callback: reflect.ValueOf(function),
+		callback: reflectFunc,
 	}
 
-	b.handlers[topic] = append(b.handlers[topic], h)
+	b.handlers[topic] = append(b.handlers[topic], handler)
 
 	go func() {
-		for v := range h.stream {
-			h.callback.Call(v)
+		for v := range handler.stream {
+			handler.callback.Call(v)
 			b.wg.Done()
 		}
 	}()
@@ -55,7 +62,7 @@ func (b *EvBus) Subscribe(topic string, function interface{}) error {
 	return nil
 }
 
-func (b *EvBus) Publish(topic string, data ...interface{}) error {
+func (b *EvBus) Publish(topic string, args ...interface{}) error {
 	hs, ok := b.handlers[topic]
 	if !ok {
 		return fmt.Errorf("%s is an unregistered topic", topic)
@@ -64,22 +71,40 @@ func (b *EvBus) Publish(topic string, data ...interface{}) error {
 	b.lock.RLock()
 	defer b.lock.RUnlock()
 
-	b.wg.Add(1)
-	go func(handlers []*evHandler, args ...interface{}) {
+	hs = append([]*evHandler{}, hs...)
+
+	b.wg.Add(len(b.handlers[topic]))
+	go func(handlers []*evHandler, args []interface{}) {
 		for _, h := range handlers {
-			h.stream <- b.convertHandlerArgs(data)
+			h.stream <- b.convertHandlerArgs(args)
 		}
-	}(hs, data)
+	}(hs, args)
 
 	return nil
 }
 
-func (b *EvBus) UnSubscribe() error {
+func (b *EvBus) UnSubscribe(topic string, function interface{}) error {
+	if reflect.TypeOf(function).Kind() != reflect.Func {
+		return fmt.Errorf("%s is not a reflect.Func", reflect.TypeOf(function))
+	}
 
+	h, i := b.findHandler(topic, reflect.ValueOf(function))
+	if h == nil {
+		return fmt.Errorf("this function is not registered in the %s", topic)
+	}
+
+	close(h.stream)
+	b.deleteTargetHandler(i, topic)
+
+	return nil
 }
 
 func (b *EvBus) Wait() {
 	b.wg.Wait()
+}
+
+func (b *EvBus) deleteTargetHandler(index int, topic string) {
+	b.handlers[topic] = append(b.handlers[topic][:index], b.handlers[topic][index+1:]...)
 }
 
 func (b *EvBus) convertHandlerArgs(args []interface{}) (hArgs []reflect.Value) {
@@ -90,4 +115,18 @@ func (b *EvBus) convertHandlerArgs(args []interface{}) (hArgs []reflect.Value) {
 	}
 
 	return hArgs
+}
+
+func (b *EvBus) findHandler(topic string, fn reflect.Value) (*evHandler, int) {
+	if _, ok := b.handlers[topic]; !ok {
+		return nil, -1
+	}
+
+	for i, h := range b.handlers[topic] {
+		if h.callback.Pointer() == fn.Pointer() {
+			return h, i
+		}
+	}
+
+	return nil, -1
 }
